89609b8b1b233c816f9dd7f9be31d57cc44abae7
lupus420 <skromny150@gmial.com>
2023-07-19 20:28:39 +0200

    Idea: permutations using bitwise manipulation

--- 1_Permutations/solution.cpp
+++ 1_Permutations/solution.cpp
@@ -36,10 +36,45 @@ public:
         }
     }
 };
+
+
+std::vector<std::vector<int>> generate_permutations() {
+    std::vector<std::vector<int>> permutations;
+    for (int A = 0; A < 2; ++A) {
+        for (int B = 0; B < 2; ++B) {
+            for (int C = 0; C < 2; ++C) {
+                int decimal_value = (A << 2) | (B << 1) | C;
+                permutations.push_back({A, B, C, decimal_value});
+            }
+        }
+    }
+    return permutations;
+}
+
 int main(){
 
     Solution s = Solution();
     vector<int> vec1 = {1,2,3};
     vector<vector<int>> my_solution = s.permute(vec1);
+
+    /*
+    Idea of creating bit manipulation and then convert bits to specific values of elements in array
+    Each 1 in binary corresponds to specific value eg.
+    array [2,5] permutations would be
+    A correspont to 2
+    B correspond to 5
+    A | B | permutation| result
+    --|---|------------|-------
+    0 | 0 |     -      |  []
+    0 | 1 |     B      |  [5]
+    1 | 0 |     A      |  [2]
+    1 | 1 |     AB     |  [2,5]
+
+    But the more bits the more nested loops 2^n, where n-nr of elements
+    */
+    std::vector<std::vector<int>> permutations = generate_permutations();
+    for (const auto& permutation : permutations) {
+        std::cout << "[" << permutation[0] << ", " << permutation[1] << ", " << permutation[2] << ", " << permutation[3] << "]" << std::endl;
+    }
     return 0;
 }
